0d15c65033eedc0d06de626cfe5fb4e2f5d2546b
Faizmohammad <42071980+Faizgeeky@users.noreply.github.com>
2021-11-05 18:21:48 +0530

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -7,15 +7,15 @@
     <input type="hidden" name="notify_url" value="localhost://pay/notify.php">  
     <br><br>Item Details<br>
     <input type="text" name="order_id" value="ItemNo12345">
-    <input type="text" name="items" value="Door bell wireless"><br>
+    <input type="text" name="items" value="Course1"><br>
     <input type="text" name="currency" value="LKR">
-    <input type="text" name="amount" value="1000">  
+    <input type="text" name="amount" value="31">  
     <br><br>Customer Details<br>
-    <input type="text" name="first_name" value="Saman">
-    <input type="text" name="last_name" value="Perera"><br>
-    <input type="text" name="email" value="samanp@gmail.com">
-    <input type="text" name="phone" value="0771234567"><br>
-    <input type="text" name="address" value="No.1, Galle Road">
+    <input type="text" name="first_name" value="Faiz">
+    <input type="text" name="last_name" value="M"><br>
+    <input type="text" name="email" value="fzoffcial429@gmail.com">
+    <input type="text" name="phone" value="8347131200"><br>
+    <input type="text" name="address" value="No.1, Demo Road">
     <input type="text" name="city" value="Colombo">
     <input type="hidden" name="country" value="Sri Lanka"><br><br> 
     <input type="submit" value="Buy Now">   
